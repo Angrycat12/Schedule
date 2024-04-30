4c89bfbd654d09c2130112b2f3da6e0f327c78f2
Angrycat <fert5737@gmail.com>
2024-04-30 23:32:28 +0300

    Good working Schedule.py

--- Schedule.py
+++ Schedule.py
@@ -12,10 +12,8 @@ class Subject:
                  name: str,
                  hours: int,
                  lesson_number_limitation: list = [],
-                 group: list[str] = ['test'],
                  difficult: bool = False,
                  test: dict = None):
-        self.group = group
         self.name = name
         self.hours = hours
         self.difficult = difficult
@@ -48,7 +46,7 @@ subjectsf: dict[str, list[int | bool]] = {'Math': [60, True],
 
 
 @cache
-def subjects(group: str) -> list[Subject]:
+def subjects() -> list[Subject]:
     """
     :return: list of subject
     """
@@ -59,8 +57,7 @@ def subjects(group: str) -> list[Subject]:
     return subject_list
 
 
-def generate(seed: int = 0,
-             school_week: int = 12,
+def generate(seed: int = 0, school_week: int = 10,
              max_nld: int = 1, min_nld: int = 1,
              group_list: list[str] = None) -> dict[int, list[list[str]]]:
     """
@@ -80,128 +77,98 @@ def generate(seed: int = 0,
         group_list = ['test']
 
     subject: Subject
+    subject_schedule_l: list[str] = []
     subject_schedule_d: list[list[str]] = []
     subject_schedule: dict[int, list[list[str]]] = {}
-    long_subject: int = 0
+    long_subject: Subject = Subject(name="Test", hours=0, difficult=False)
     all_hours: int = 0
-    max_lesson_d: int = 0
+    max_lesson_d: int = 9
     seed_l: list[int] = []
-    previous_subject = 0
+    previous_subject: str = 'None'
+    group = Group(name='Test', subjects=subjects())
+    groups: list[Group] = [group]
 
-    def schedule_logic(day_subject_list: list,
-                       group: str) -> list[str]:
-        subject_schedule: list = []
-        subject_index: int = 0
-        lessons: int = 0
-        previous_subject: int = 0
-
-        for lessons, subject_index in enumerate(day_subject_list):
-            if lessons == 0:
-                print('lesson == 0')
-                if subjects(group)[subject_index].difficult == False and \
-                        subjects(group)[subject_index].hours > 0 and \
-                        previous_subject == subject_index:
-                    print('subject_index: ', subject_index)
-                    subject_schedule.append(subjects(group)[subject_index].name)
-                    r: Subject = subjects(group)[subject_index]
-                    subjects(group).remove(subjects(group)[subject_index])
-                    subjects(group).append(Subject(name=r.name, hours=r.hours - 1, difficult=r.difficult))
-                else:
-                    for subject in subjects(group):
-                        if subject.hours > subjects(group)[subject_index].hours and subject.difficult == False:
-                            if subject.hours > 0 and previous_subject == subject_index:
-                                subject_index = subjects(group).index(subject)
-                    print('subject_index: ', subject_index)
-
-                    subject_schedule.append(subjects(group)[subject_index].name)
-                    r = subjects(group)[subject_index]
-                    subjects(group).remove(subjects(group)[subject_index])
-                    subjects(group).append(Subject(name=r.name, hours=r.hours - 1, difficult=r.difficult))
-            else:
-                print('lesson == any')
-                print('subject_index: ', subject_index)
-                if subjects(group)[subject_index].hours > 0:
-                    subject_schedule.append(subjects(group)[subject_index].name)
-                    r = subjects(group)[subject_index]
-                    subjects(group).remove(subjects(group)[subject_index])
-                    subjects(group).append(Subject(name=r.name, hours=r.hours - 1, difficult=r.difficult))
-            previous_subject = subject_index
-
-        return subject_schedule
-
-    for group in group_list:
-
-        for subject in subjects(group):
-            if group in subject.group:
-                all_hours = subject.hours + all_hours
-                max_lesson_d: int = math.ceil(all_hours / (5 * school_week))  # max lessons on day
-        print(f'max_lesson_d: {max_lesson_d}')
-        print(f'all_hours: {all_hours}')
-        print(f'5*school_week: {5 * school_week}')
-
-        if seed == 1:
-            # generating numbers the size of the number of lessons per day
-            seed = random.randint(10 ** max_lesson_d, 10 ** (max_lesson_d + 1) - 1)
-            seed_l = list(map(int, str(seed)))
-        elif seed > 1:
-            if 10 ** max_lesson_d < seed:
-                if seed < 10 ** (max_lesson_d + 1) - 1:
-                    seed_l = list(map(int, str(seed)))
-                else:
-                    seed_l = list(map(int, str(seed)))
-                    seed = seed / (10 ** (len(seed_l) - max_lesson_d))  # сделать через div
-            else:
-                seed = seed * (10 ** (max_lesson_d - len(list(map(int, str(seed))))))
+    if seed == 1:
+        # generating numbers the size of the number of lessons per day
+        seed = random.randint(10 ** max_lesson_d, 10 ** (max_lesson_d + 1) - 1)
+        seed_l = list(map(int, str(seed)))
+    elif seed > 1:
+        if 10 ** max_lesson_d < seed:
+            if seed < 10 ** (max_lesson_d + 1) - 1:
                 seed_l = list(map(int, str(seed)))
+            else:
+                seed_l = list(map(int, str(seed)))
+                seed = seed / (10 ** (len(seed_l) - max_lesson_d))  # сделать через div
+        else:
+            seed = seed * (10 ** (max_lesson_d - len(list(map(int, str(seed))))))
+            seed_l = list(map(int, str(seed)))
 
-        for week in range(school_week):
-            print(f'week: {week}')
+    for week in range(school_week):
+        print(f'week: {week}')
 
-            for day in range(5):
-                print(f'day: {day}')
+        for day in range(5):
+            print(f'day: {day}')
 
-                if seed == 0:
-                    for lessons in range(max_lesson_d):
-                        print(f'lessons: {lessons}')
+            for group in groups:
+                print(f'group: {group.name}')
 
-                        for subject in subjects(group):
-                            if subject.hours > 0:
-                                if lessons == 0:
-                                    print('es')
-                                    if subject.hours > subjects(group)[long_subject].hours:
-                                        long_subject = subjects(group).index(subject)
-                                else:
-                                    print('nt')
-                                    if subject.hours > subjects(group)[long_subject].hours and previous_subject != long_subject:
-                                        long_subject = subjects(group).index(subject)
+                for lesson in range(max_lesson_d):
+                    print(f'lesson: {lesson}')
 
-                        seed_l.append(long_subject)
-                        previous_subject = long_subject
-                        print('seed: ', seed_l)
-                        print('long_subject: ', long_subject)
-                        print('previous_subject: ', previous_subject)
+                    long_subject = group.subjects[0]
 
-                print('seed d: ', seed_l)
+                    for subject in group.subjects:
+                        print(f'subject: {subject.name}')
+                        print(f'{subject.hours}')
 
-                subject_schedule_d.append(schedule_logic(seed_l, group))
-                seed_l = []
-                seed = 0
+                        if subject.hours > 0:
+                            if lesson == 0:
+                                if subject.difficult == False:
+                                    if subject.hours > long_subject.hours and subject.name != previous_subject:
+                                        long_subject = subject
+                            else:
+                                if subject.hours > long_subject.hours and subject.name != previous_subject:
+                                    long_subject = subject
+                        else:
+                            group.subjects.remove(subject)
 
-            subject_schedule[week] = subject_schedule_d
-            subject_schedule_d = []
+                    if len(group.subjects) == 0:    #кастыль
+                        group.subjects.append(Subject(name='None', hours=1000))
 
-        print(all_hours / (5 * school_week))
-        print(f'Group: {group}')
-        for subject in subjects(group):
-            print(f'Name: {subject.name} \n'
-                  f'Hours: {subject.hours}')
+                    if long_subject.name != previous_subject:
+                        previous_subject = long_subject.name
+
+                    elif long_subject.name == 'None':   #часть костыля
+                        previous_subject = long_subject.name
+
+                    else:
+                        long_subject = group.subjects[random.randint(0, len(group.subjects)-1)]
+                        previous_subject = long_subject.name
+
+                    print(f'Subject in this lesson: {long_subject.name}')
+
+                    group.subjects.remove(long_subject)
+                    long_subject.hours -= 1
+                    group.subjects.append(long_subject)
+
+                    subject_schedule_l.append(long_subject.name)
+
+                # subject_schedule_g[group] = subject_schedule_l
+                # subject_schedule_l = []
+
+            subject_schedule_d.append(subject_schedule_l)   # subject_schedule_g
+            # subject_schedule_g = {}
+            subject_schedule_l = []
+
+        subject_schedule[week] = subject_schedule_d
+        subject_schedule_d = []
 
     return subject_schedule
 
 
 if __name__ == '__main__':
     def main() -> None:
-        seed = int(input())
+        #seed = int(input())
         print(generate())
 
     main()  # 1- доделать генерацию 2- добавить работу max_nld и min_nld
